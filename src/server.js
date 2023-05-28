0b6e146772a99956bdaafdc92a905f952472e36c
junhui324 <junhuijang00@gmail.com>
2023-05-29 03:50:22 +0900

    :construction: 아이디 및 방제목 입력 동시에 하기
    아이디 및 방제목을 동시에 입력한 후 채팅방에 들어갈 수 있도록 코드 수정

---
+++ src/server.js
@@ -0,0 +1,46 @@
+import http from 'http';
+import express from 'express';
+import SocketIo from 'socket.io';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views`);
+app.use('/public', express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
+
+const httpServer = http.createServer(app);
+const wsServer = SocketIo(httpServer);
+
+wsServer.on('connection', (socket) => {
+  socket['nickname'] = 'Anonymous';
+  socket.onAny((event) => console.log(`Socket Event: ${event}`));
+
+  socket.on('enter_room', (roomName, nickname, done) => {
+    socket['nickname'] = nickname;
+    socket.join(roomName);
+    socket.to(roomName).emit('welcome', socket.nickname);
+    done();
+  });
+
+  socket.on('disconnecting', () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit('bye', socket.nickname)
+    );
+  });
+
+  socket.on('new_message', (msg, room, done) => {
+    socket.to(room).emit('new_message', `${socket.nickname}: ${msg}`);
+    done();
+  });
+
+  socket.on('nickname', (nickname) => (socket['nickname'] = nickname));
+});
+
+const handleListen = () => {
+  console.log('✔Server running on http://localhost:3000');
+};
+
+httpServer.listen(3000, handleListen);
